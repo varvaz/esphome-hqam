35f86603e2f1f37441fe15fceb4503bbcf03be1f
Bokenbergen <75536984+Bokenbergen@users.noreply.github.com>
2025-04-11 20:05:10 +0200

    Update automower.h

--- components/automower/automower.h
+++ components/automower/automower.h
@@ -1,9 +1,6 @@
 #pragma once
 
-#include "esphome/core/component.h"
-#include "esphome/components/uart/uart.h"
-#include "esphome/components/sensor/sensor.h"
-#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome.h"
 
 namespace esphome {
 namespace automower {

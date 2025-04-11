acc4ca71c7f66729a603e58abb62800f0ef1eb50
Bokenbergen <75536984+Bokenbergen@users.noreply.github.com>
2025-04-11 19:45:14 +0200

    Update __init__.py

--- components/confs/__init__.py
+++ components/confs/__init__.py
@@ -1,2 +1,14 @@
 import esphome.codegen as cg
-custom_ns = cg.esphome_ns.namespace("confs")
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+confs_ns = cg.esphome_ns.namespace('confs')
+ConfsComponent = confs_ns.class_('ConfsComponent', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ConfsComponent),
+}).extend(cv.COMPONENT_SCHEMA)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)

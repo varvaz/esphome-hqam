6977b63a81995ddd3d7fce6a1de0ae084839160b
Bokenbergen <75536984+Bokenbergen@users.noreply.github.com>
2025-04-11 20:53:17 +0200

    Update __init__.py

--- components/automower/__init__.py
+++ components/automower/__init__.py
@@ -1,17 +1,1 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
 
-from esphome import automation
-from esphome.const import CONF_ID
-
-# Create namespace and define component class
-automower_ns = cg.esphome_ns.namespace('automower')
-AutomowerComponent = automower_ns.class_('AutomowerComponent', cg.Component)
-
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(AutomowerComponent),
-}).extend(cv.COMPONENT_SCHEMA)
-
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)

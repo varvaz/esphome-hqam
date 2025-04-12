e0d2f088cfec6cc182f66bfedeb809512a1e1cd1
Bokenbergen <75536984+Bokenbergen@users.noreply.github.com>
2025-04-12 14:00:49 +0200

    Update __init__.py

--- components/mower/__init__.py
+++ components/mower/__init__.py
@@ -1,17 +1,21 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 
-from esphome import automation
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL
+from esphome.components import uart
 
-# Correct namespace and class name
-mower_ns = cg.esphome_ns.namespace('mower')
-Automower = mower_ns.class_('Automower', cg.Component)
+# Create the namespace and component class.
+mower_ns = cg.esphome_ns.namespace("mower")
+Automower = mower_ns.class_("Automower", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(Automower),
+    cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
+    cv.Required(CONF_UPDATE_INTERVAL): cv.positive_time_period_milliseconds,
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
+    uart_var = config["uart_id"]
+    update_interval = config[CONF_UPDATE_INTERVAL]
+    var = cg.new_Pvariable(config[CONF_ID], uart_var, update_interval)
+    cg.register_component(var, config)
